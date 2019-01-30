c02158fc7a774fe02e94830af98eb3bd16d358bb
James Brown <jamesphone180@gmail.com>
2019-01-30 21:27:25 +1300

    Added stubs for temp sensing

---
+++ SPILib/QT_pcbtemp.c
@@ -0,0 +1,19 @@
+/*
+ * QT_pcbtemp.c
+ *
+ *  Created on: 30/01/2019
+ *      Author: james
+ */
+
+#include "QT_pcbtmp.h"
+
+void QT_TEMP_initialise() {
+
+}
+
+/**
+ * This function reads the temperature of the PCB, this function may block if the temperature reading is not ready yet.
+ */
+float QT_TEMP_readTemperature() {
+
+}
